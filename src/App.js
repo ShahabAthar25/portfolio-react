ce029f8eb337cd5f918795967b19e860a8e43693
ShahabAthar25 <shahabathar25@gmail.com>
2022-10-04 20:01:21 +0500

    Add particle ts

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Background from "./components/Background";
+
+function App() {
+  return (
+    <div className="h-screen w-screen">
+      <Background />
+      <h1 className="text-7xl">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Background.js
@@ -0,0 +1,82 @@
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
+export default function Background() {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        background: {
+          color: {
+            value: "#061E3B",
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 1,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            enable: true,
+            speed: 1,
+            direction: "top-right",
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 1080,
+            },
+            limit: 0,
+            value: 70,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  );
+}
